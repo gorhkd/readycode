a664d6f47a9890f03e35a03142b6e86ce982786c
gorhkd <gorhkd223@gmail.com>
2025-05-14 23:51:20 +0900

    init: 프로젝트 초기 설정 및 GitHub 연결

---
+++ src/main/java/com/ll/readycode/ReadycodeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.readycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadycodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/readycode/ReadycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.readycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
