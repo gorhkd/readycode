47805878661033163b03b9482c209c4e32ee7278
gorhkd <gorhkd223@gmail.com>
2025-05-15 00:58:30 +0900

    init: 기본 yml 세팅 및 dependency 추가

--- backend/src/main/java/com/ll/readycode/ReadycodeApplication.java
+++ backend/src/main/java/com/ll/readycode/ReadycodeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReadycodeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReadycodeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReadycodeApplication.class, args);
+    }
 
 }
