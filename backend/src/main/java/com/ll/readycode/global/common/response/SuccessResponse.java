7e62d7a3f6f0222f2739984988f229e320794b81
gorhkd <gorhkd223@gmail.com>
2025-05-19 11:50:10 +0900

    feat: SuccessResponse 응답 포맷 도입 및 제네릭 구조 구성

---
+++ backend/src/main/java/com/ll/readycode/global/common/response/SuccessResponse.java
@@ -0,0 +1,20 @@
+package com.ll.readycode.global.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SuccessResponse<T> {
+    private final int status;
+    private final String message;
+    private final T data;
+
+    public static <T> SuccessResponse<T> of(T data) {
+        return new SuccessResponse<>(200, "요청이 성공했습니다.", data);
+    }
+
+    public static <T> SuccessResponse<T> of(String message, T data) {
+        return new SuccessResponse<>(200, message, data);
+    }
+}
