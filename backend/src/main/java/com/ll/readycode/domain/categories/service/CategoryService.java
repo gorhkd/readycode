a13e973e04e605651ab16551c2d334b6037852c2
gorhkd <gorhkd223@gmail.com>
2025-07-01 23:37:46 +0900

    feat: 카테고리 등록/조회/삭제 서비스 로직 구현

--- backend/src/main/java/com/ll/readycode/domain/categories/service/CategoryService.java
+++ backend/src/main/java/com/ll/readycode/domain/categories/service/CategoryService.java
@@ -6,15 +6,59 @@ import com.ll.readycode.global.exception.CustomException;
 import com.ll.readycode.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CategoryService {
   private final CategoryRepository categoryRepository;
 
-  public Category findCategoryById(Long id) {
+  @Transactional(readOnly = true)
+  public List<CategoryResponse> getAllCategories() {
+    return categoryRepository.findAll().stream().map(CategoryResponse::from).toList();
+  }
+
+  @Transactional
+  public CategoryResponse createCategory(CategoryRequest request) {
+    validateDuplicateName(request.name());
+
+    Category category = Category.builder().name(request.name()).build();
+
+    categoryRepository.save(category);
+    return CategoryResponse.from(category);
+  }
+
+  @Transactional
+  public CategoryResponse updateCategory(Long categoriesId, CategoryRequest request) {
+    Category category = findCategoryById(categoriesId);
+
+    if (!category.getName().equals(request.name())) {
+      validateDuplicateName(request.name());
+      category.updateName(request.name());
+    }
+
+    return CategoryResponse.from(category);
+  }
+
+  @Transactional
+  public void deleteCategory(Long categoriesId) {
+    Category category = findCategoryById(categoriesId);
+    categoryRepository.delete(category);
+  }
+
+  @Transactional(readOnly = true)
+  private void validateDuplicateName(String name) {
+    if (categoryRepository.existsByName(name)) {
+      throw new CustomException(ErrorCode.DUPLICATE_CATEGORY);
+    }
+  }
+
+  @Transactional(readOnly = true)
+  public Category findCategoryById(Long categoriesId) {
     return categoryRepository
-        .findById(id)
+        .findById(categoriesId)
         .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
   }
 }
