47f42a1984c4067c6097c820ede834723f1fab23
gorhkd <gorhkd223@gmail.com>
2025-07-16 00:03:05 +0900

    chore: Swagger @Schema 적용 - Category DTO

--- backend/src/main/java/com/ll/readycode/api/categories/dto/request/CategoryRequest.java
+++ backend/src/main/java/com/ll/readycode/api/categories/dto/request/CategoryRequest.java
@@ -1,3 +1,5 @@
 package com.ll.readycode.api.categories.dto.request;
 
-public record CategoryRequest(String name) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record CategoryRequest(@Schema(description = "카테고리 이름", example = "백엔드") String name) {}
