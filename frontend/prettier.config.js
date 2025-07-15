cfeebb0f4f25aaf128979702a21bdf6c4c4c62c4
김애진 <77660676+code-zeen@users.noreply.github.com>
2025-07-15 22:33:53 +0900

    chore: 프리티어 설정

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -2,9 +2,14 @@
 
 /** @type {import('prettier').Config} */
 const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  tabWidth: 2,
+  trailingComma: 'all',
+}
 
 export default config;
