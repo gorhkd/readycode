cea51e14ea48cdfe52af3f736388b72533bf23e0
김애진 <77660676+code-zeen@users.noreply.github.com>
2025-07-18 15:12:25 +0900

    feat: axios 인스턴스 기본 설정으로 생성

---
+++ frontend/src/api/axiosConfig.ts
@@ -0,0 +1,40 @@
+import axios, { type AxiosResponse } from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+interface APIResponse<T = never> {
+  metaData: any
+  data: T
+}
+
+const api = {
+  get: async <T = never>(url: string, config?: object): Promise<AxiosResponse<APIResponse<T>>> =>
+    axiosInstance.get<APIResponse<T>>(url, config),
+  post: async <T = never>(
+    url: string,
+    data?: object,
+    config?: object,
+  ): Promise<AxiosResponse<APIResponse<T>>> =>
+    axiosInstance.post<APIResponse<T>>(url, data, config),
+  put: async <T = never>(
+    url: string,
+    data?: object,
+    config?: object,
+  ): Promise<AxiosResponse<APIResponse<T>>> => axiosInstance.put<APIResponse<T>>(url, data, config),
+  patch: async <T = never>(
+    url: string,
+    data?: object,
+    config?: object,
+  ): Promise<AxiosResponse<APIResponse<T>>> =>
+    axiosInstance.patch<APIResponse<T>>(url, data, config),
+  delete: async <T = never>(url: string, config?: object): Promise<AxiosResponse<APIResponse<T>>> =>
+    axiosInstance.delete<APIResponse<T>>(url, config),
+}
+
+export default api
