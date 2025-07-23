2b8bb8677c21e3bdd5cb7642370af9e83f886b7d
김애진 <77660676+code-zeen@users.noreply.github.com>
2025-07-24 00:51:00 +0900

    feat: 라이트모드 테마 변경

--- frontend/src/components/common/navbar/ThemeToggleButton.tsx
+++ frontend/src/components/common/navbar/ThemeToggleButton.tsx
@@ -3,7 +3,7 @@ import { MoonIcon, SunIcon } from '@phosphor-icons/react'
 export default function ThemeToggleButton() {
   return (
     <label className="swap swap-rotate">
-      <input type="checkbox" className="theme-controller" value="nord" />
+      <input type="checkbox" className="theme-controller" value="bumblebee" />
 
       <SunIcon className="swap-on h-6 w-6" />
       <MoonIcon className="swap-off h-6 w-6" />
