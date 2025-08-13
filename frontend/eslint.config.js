8730c11c21f6f47bf871609fd380dbc6097025a4
김애진 <77660676+code-zeen@users.noreply.github.com>
2025-08-13 14:08:12 +0900

    chore: ESLint 관련 세팅 수정

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,4 +2,11 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      'pnpm/json-enforce-catalog': 'off'
+    }
+  }
+]
