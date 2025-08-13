7f08e7a6971a5038e9e4077976290493e5a05984
김애진 <77660676+code-zeen@users.noreply.github.com>
2025-08-13 17:58:38 +0900

    feat: 템플릿 CRUD 관련 모든 fetch 함수 준비

--- frontend/src/api/templateApi.ts
+++ frontend/src/api/templateApi.ts
@@ -1,10 +1,55 @@
-import type {operations} from '@/api/dto.ts'
+import type { operations } from '@/api/dto.ts'
 import api from '@/api/axiosConfig.ts'
 
+// 조회
 export type GetTemplatesQuery = operations['getTemplates']['parameters']['query']
 type GetTemplatesResponse = operations['getTemplates']['responses'][200]['content']['*/*']
+// 생성
+export type CreateTemplatesRequest = operations['createTemplate']['requestBody']['content']['application/json']
+type CreateTemplateResponse = operations['createTemplate']['responses'][200]['content']['*/*']
+// 개별 조회
+export type GetDetailsTemplatePath = operations['detailsTemplate']['parameters']['path']['templatesId']
+type GetDetailsTemplateResponse = operations['detailsTemplate']['responses'][200]['content']['*/*']
+// 개별 삭제
+export type DeleteTemplatePath = operations['deleteTemplate']['parameters']['path']['templatesId']
+type DeleteTemplateResponse = operations['deleteTemplate']['responses'][200]['content']['*/*']
+// 개별 수정
+export type ModifyTemplatePath = operations['modifyTemplate']['parameters']['path']['templatesId']
+type ModifyTemplateRequest = operations['modifyTemplate']['requestBody']['content']['application/json']
+type ModifyTemplateResponse = operations['modifyTemplate']['responses'][200]['content']['*/*']
 
+// 조회
 export async function getTemplates(params: GetTemplatesQuery = {}) {
-    const { data } = await api.get<GetTemplatesResponse>('/templates', { params })
-    return data
+  const { data } = await api.get<GetTemplatesResponse>('/templates', { params })
+  return data
+}
+
+// 생성
+export async function createTemplate(requestBody: CreateTemplatesRequest = {
+  title: '테스트 템플릿 1',
+  description: '테스트 설명글',
+  price: 1000,
+  categoryId: 1,
+  image: 'imageString'
+}) {
+  const { data } = await api.post<CreateTemplateResponse>('/templates', requestBody)
+  return data
+}
+
+// 개별 조회
+export async function getTemplateById(id: GetDetailsTemplatePath) {
+  const { data } = await api.get<GetDetailsTemplateResponse>(`/templates/${id}`)
+  return data
+}
+
+// 개별 삭제
+export async function deleteTemplateById(id: DeleteTemplatePath) {
+  const { data } = await api.delete<DeleteTemplateResponse>(`/templates/${id}`)
+  return data
+}
+
+// 개별 수정
+export async function modifyTemplateById(id: ModifyTemplatePath, requestBody: ModifyTemplateRequest) {
+  const { data } = await api.patch<ModifyTemplateResponse>(`/templates/${id}`, requestBody)
+  return data
 }
